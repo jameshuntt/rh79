11ef35f25dc94082aac07ef947c76c7694e799c5
jameshuntt <jh222807@live.sunysccc.edu>
2019-08-21 02:52:59 -0400

    took out last bits of inline styling and changed the navbar components, changed about to event information, basically edited navbar and the scss file associated with it

--- client/src/components/CustomNavbar/CustomNavbar.jsx
+++ client/src/components/CustomNavbar/CustomNavbar.jsx
@@ -8,31 +8,33 @@ import './CustomNavbar.scss'
 export default class CustomNavbar extends Component {
     render() {
         return (
-            <div id="container" style={{ width: '100vw', height: '8vh', background: 'black', opacity: '.8', display: 'flex', flexDirection: 'row' }}>
-                    <div style={{ display: 'flex', marginRight: '0' }}>
-                        <h3 className="navbar-brand">Rory Healy Memorial Golf Tournament</h3>
-                        {/*<Link to="/"><AwesomeButton style={{ paddingTop: '1vh', paddingRight: '1vw', paddingBottom: '1vh', paddingLeft: '1vw' }} type="primary">Home</AwesomeButton></Link>
-                        <Link to="/About"><AwesomeButton style={{ paddingTop: '1vh', paddingRight: '1vw', paddingBottom: '1vh', paddingLeft: '1vw' }} type="primary">About</AwesomeButton></Link>*/}
+            <div className="navbar-container">
+                    <div className="navbar-brand-container">
+                        <h3 className="navbar-brand">
+                            Rory Healy Memorial Golf Tournament
+                        </h3>
                     </div>
-                    <div style={{ display: 'flex' }} className="spacer" />
-                    <div style={{ display: 'flex', margin: '0', padding: '0' }}>
-                        <img style={{ height: '7vh', width: '13vh' }} src="/assets/GHS79.png" />
+                    <div className="spacer" />
+                    <div className="navbar-logo-container">
+                        <img className="navbar-logo" src="/assets/GHS79.png" />
                     </div>
-                    <div style={{ display: 'flex' }} className="spacer" />
+                    <div className="spacer" />
                     <div>
-                        {/*<Dropdown style={{zIndex: '500'}}>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Dropdown Button
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-        </Dropdown>*/}
-                        <Link to="/SignUp"><AwesomeButton style={{ paddingTop: '1vh', paddingRight: '1vw', paddingBottom: '1vh', paddingLeft: '1vw' }} type="primary">Sign up</AwesomeButton></Link>
-                        <Link to="/Sponsors"><AwesomeButton style={{ paddingTop: '1vh', paddingRight: '1vw', paddingBottom: '1vh', paddingLeft: '1vw' }} type="primary">Sponsors</AwesomeButton></Link>
+                        <Link to="/">
+                            <AwesomeButton className="navbar-button" type="primary">
+                                Home
+                            </AwesomeButton>
+                        </Link>
+                        <Link to="/EventInformation">
+                            <AwesomeButton className="navbar-button" type="primary">
+                                Event information
+                            </AwesomeButton>
+                        </Link>
+                        <Link to="/Register">
+                            <AwesomeButton className="navbar-button" type="primary">
+                                Register
+                            </AwesomeButton>
+                        </Link>
                     </div>
             </div>
         )
