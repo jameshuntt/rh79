db6a207d7d155cfcd5d9bcb965ae2d2c49d99ff4
jameshuntt <jh222807@live.sunysccc.edu>
2019-08-21 03:46:36 -0400

    added folder for event information and both jsx and scss files within that folder

---
+++ client/src/components/EventInformation/EventInformation.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './EventInformation.scss'
+
+export default class EventInformation extends Component {
+    render() {
+        return (
+            <div className="event-information-container">
+                
+            </div>
+        )
+    }
+}
