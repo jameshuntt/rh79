e0f902bd54023a29497699586cd96d04aaa80176
jameshuntt <jh222807@live.sunysccc.edu>
2019-08-21 03:43:18 -0400

    deleted unused routes from app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import CustomNavbar from './components/CustomNavbar/CustomNavbar';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
-//import SignUp from './components/SignUp/SignUp';
-//import SignIn from './components/SignIn/SignIn';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About';
 import Checkout from './components/Checkout/Checkout';
@@ -19,8 +17,6 @@ export default class App extends Component {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/EventInformation" component={About} />
-                    {/*<Route path="/SignUp" component={SignUp} />
-                    <Route path="/SignIn" component={SignIn} />*/}
                     <Route path="/Contact" component={Contact} />
                     <Route path="/Sponsors" component={Sponsors} />
                     <Route path="/Register" component={Checkout} />
