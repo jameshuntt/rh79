c406532febb68b7dcbeefd6e2dd09525f289013a
jameshuntt <jh222807@live.sunysccc.edu>
2019-08-21 04:41:10 -0400

    added paragraph to the sponsors page

--- client/src/components/Sponsors/Sponsors.jsx
+++ client/src/components/Sponsors/Sponsors.jsx
@@ -5,6 +5,7 @@ export default class Sponsors extends Component {
         return (
             <div className="containers">
                 <h3>GHS79 Golf is proudly sponsired by some of the finest individuals from many backgrounds.</h3>
+                <p>list will be coming soon</p>
             </div>
         )
     }
