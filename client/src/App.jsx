915ff681eb59d1db38ad205423d28f708730f597
jameshuntt <jh222807@live.sunysccc.edu>
2019-08-21 03:54:44 -0400

    added new route to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,12 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import CustomNavbar from './components/CustomNavbar/CustomNavbar';
 import Home from './components/Home/Home';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import EventInformation from './components/EventInformation/EventInformation';
 import Checkout from './components/Checkout/Checkout';
 import Sponsors from './components/Sponsors/Sponsors';
+import Footer from './components/Footer/Footer';
 
 export default class App extends Component {
     render() {
@@ -16,10 +17,11 @@ export default class App extends Component {
                 <CustomNavbar />
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/EventInformation" component={About} />
+                    <Route path="/About" component={About} />
                     <Route path="/Contact" component={Contact} />
-                    <Route path="/Sponsors" component={Sponsors} />
+                    <Route path="/EventInformation" component={EventInformation} />
                     <Route path="/Register" component={Checkout} />
+                    <Route path="/Sponsors" component={Sponsors} />
                 </Switch>
                 <Footer />
             </div>
